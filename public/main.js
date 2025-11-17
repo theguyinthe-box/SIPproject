15c35673652d82ff961156e8661e57e1d064c8e4
BUNN1E5 <bunn1e5@users.noreply.github.com>
2025-11-17 10:22:25 -0800

    Random image and some requests are working

--- public/main.js
+++ public/main.js
@@ -1,12 +1,54 @@
-document.addEventListener('keydown', function(event) {
-    if(event.key == 'ArrowLeft') {
+let imageFilename = "";
+
+window.onload = function () {
+    getRandomImage();
+}
+
+
+document.addEventListener('keydown', function (event) {
+    if (event.key == 'ArrowLeft') {
         alert('Left was pressed');
     }
-    else if(event.key == 'ArrowRight') {
+    else if (event.key == 'ArrowRight') {
         alert('Right was pressed');
     }
 });
 
-function makeSelection(userID, imageShown, userSelection){
+async function makeSelection(userSelection) {
+    try {
+        const response = await fetch('http://localhost:3000/userInput', {
+            method: 'POST', 
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                selection: userSelection,
+                filename: imageFilename
+            })
+        });
+        await getRandomImage();
+        return response;
+    } catch (error) {
+        console.error('Error:', error);
+        alert("Error sending request: " + error);
+    }
+    
+}
+
+async function getRandomImage() {
+    try {
+        const response = await fetch('http://localhost:3000/randomImage', {
+            method: "GET",
+        });
+        imageFilename = response.headers.get('X-Image-Filename');
+        const baseUrl = response.url;
+        const timestamp = new Date().getTime();
+        const uniqueUrl = `${baseUrl}?t=${timestamp}`;
+        document.getElementById("rand_img").src = uniqueUrl;
         
+        return response;
+    } catch (error) {
+        console.error('Error:', error);
+        alert("Error sending request: " + error);
+    }
 }
