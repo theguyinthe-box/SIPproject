c959d9edc61f3549bc5caf9a53cafc4d0316addc
BUNN1E5 <BUNN1E5@users.noreply.github.com>
2025-11-18 17:15:23 -0800

    got the GUI working under Docker

--- startTheShow.bat
+++ startTheShow.bat
@@ -1,7 +1,7 @@
 @echo off
 echo Starting Program ...
 set CONTAINER_NAME="sips"
-docker run -d -p 3000:80 --name %CONTAINER_NAME% --volume .\data --gpus all SIPS_Container
+docker run -d -p 80:3000 --name %CONTAINER_NAME% --volume .\data --gpus all SIPS_Container
 start /wait chrome "http://localhost"
 docker exec -it %CONTAINER_NAME% bash -c "sh ~/src/scripts/exit.sh"
 docker stop %CONTAINER_NAME%
