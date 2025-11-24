5cafbdcb3b2c040113aaada346defceb56d67f91
theguyinthe_box <dkaauwai0821@sdsu.edu>
2025-11-24 15:20:48 -0800

    added load aligned func in img handler

--- src/model/imageHandler.py
+++ src/model/imageHandler.py
@@ -1,13 +1,18 @@
 import glob,os
 import PIL.Image
 
-img_path = "../shared"
-valid_images = (".jpg",".png")
+RAW_IMG_PATH = "../shared"
+ALIGNED_IMG_PATH = "../shared/aligned"
+VALID_IMG_EXT = (".jpg",".png")
 
 def load_raw():
-    for f in os.listdir(img_path):
-        if f.endswith(valid_images):
-            img = PIL.Image.open(img_path + f)
+    for f in os.listdir(RAW_IMG_PATH):
+        if f.endswith(VALID_IMG_EXT):
+            img = PIL.Image.open(RAW_IMG_PATH + f)
     return img
 
-
+def load_aligned():
+    for f in os.listdir(ALIGNED_IMG_PATH):
+        if f.endswith(VALID_IMG_EXT):
+            img = PIL.Image.open(ALIGNED_IMG_PATH + f)
+    return img
