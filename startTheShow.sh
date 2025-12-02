ac9e1f5d8fb193fc33f5cec1413d2c506711e85f
BUNN1E5 <bunn1e5@users.noreply.github.com>
2025-12-02 06:32:11 -0800

    back to firefox, I just had tabs opened already...

--- startTheShow.sh
+++ startTheShow.sh
@@ -6,7 +6,7 @@ pkill adb
 echo Starting Program...
 docker run --gpus all --name sips -itd -p 3000:3000 --privileged -v /dev/bus/usb:/dev/bun/usb -v ./src/shared/results:/src/shared/results sips
 docker exec sips scripts/entrypoint.sh
-brave "http://localhost:3000" &
+firefox "http://localhost:3000" &
 docker exec sips node /src/SIP-GUI/main.js
 echo "waiting for firefox to close"
 docker exec sips scripts/start_gui.sh
