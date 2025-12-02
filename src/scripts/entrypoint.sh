5bb8410e891584f2e1cc39c0aaf36d8177f22b8e
BUNN1E5 <bunn1e5@users.noreply.github.com>
2025-12-02 04:02:13 -0800

    remmoved cache files, almost working entrypoint

--- src/scripts/entrypoint.sh
+++ src/scripts/entrypoint.sh
@@ -2,22 +2,26 @@
 SCRIPT_PATH=$(readlink -f "$0")
 SCRIPT_DIR=$(dirname "$SCRIPT_PATH")
 SRC_DIR=$(cd "$SCRIPT_DIR/.." && pwd)
-SHARED_DIR="{$SRC_DIR}/shared"
-sh {$SCRIPT_DIR}/clear_shared_folders.sh
+sh $SCRIPT_DIR/clear_shared_folders.sh
 
-#adb pull sdcard/DCIM/Camera {$SRC_DIR}/shared/
-#adb shell rm sdcard/DCIM/Camera/*
+adb pull sdcard/DCIM/Camera $SRC_DIR/shared/
+adb shell rm sdcard/DCIM/Camera/*
 
 #go to main.py directory
 # run the program
-python {$SRC_DIR}/model/main.py
+python $SRC_DIR/model/main.py
 
-mv $SHARED_DIR/altered/* $SHARED_DIR/test/
-for i in {1..5}; do
-    cp $SHARED_DIR/aligned/* $SHARED_DIR/test/og_aligned_$i.png
+echo "moving altered files to test dir..."
+mv $SRC_DIR/shared/altered/* $SRC_DIR/shared/test/
+
+echo "creating 5 copies of original..."
+for i in $(seq 1 5); do
+    cp $SRC_DIR/shared/aligned/* $SRC_DIR/shared/test/original_aligned_$i.png
 done
-rm cp $SHARED_DIR/aligned/*
-rm cp $SHARED_DIR/alignment_vector/*
+
+echo "deleting any leftover images..."
+rm $SRC_DIR/shared/aligned/*
+rm $SRC_DIR/shared/alignment_vector/*
 
 # run the GUI
 #node src/SIP_GUI/main.js
