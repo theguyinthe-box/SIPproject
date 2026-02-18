90803039f922b63d770d55b1373814ed925133ac
BUNN1E5 <bunn1e5@users.noreply.github.com>
2026-02-17 19:16:46 -0800

    Committing cause I accidently overwrite my .zshrc and need to recover...

--- SIP-GUI/main.js
+++ SIP-GUI/main.js
@@ -1,29 +1,14 @@
-// const { app, BrowserWindow } = require('electron')
-const fs = require('fs');
 const path = require('path');
-const exifr = require('exifr');
 
 const express = require('express');
 const app = express();
 const port = 3000;
 
-let subjectID = null;
-
-//Needs:
-//User input for subject ID
-//Randomly select an image from test dataset
-//Display image to user
-//Allow user to make selection from predefined set of options
-//File name is SubjectID.csv
-//Record subject ID, image shown, and user selection to a local file
+//let subjectID = null;
 
 const publicDir = path.resolve(__dirname, 'public');
-const sharedDir = path.resolve(__dirname, '..', 'shared');
 
 app.use(express.static(publicDir));
-//app.use(express.static(sharedDir));
-
-console.log('Serving static files from:', publicDir);
 app.use(express.json());
 
 app.get('/', (req, res) => {
@@ -32,4 +17,11 @@ app.get('/', (req, res) => {
 
 let server = app.listen(port, () => {
   console.log(`app listening on port ${port}`)
+})
+
+app.get('/closeApp', (req, res) => {
+  console.log("Closing App as per user request.");
+  server.close();
+  res.json({ success: true });
+  process.exit(0);
 })
