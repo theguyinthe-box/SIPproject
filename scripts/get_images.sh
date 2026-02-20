7cb2c8df56529f020dceda8c3d9d982f5a73e859
BUNN1E5 <bunn1e5@users.noreply.github.com>
2026-02-19 22:08:50 -0800

    added readme fixed bad path of get_images

--- scripts/get_images.sh
+++ scripts/get_images.sh
@@ -2,5 +2,5 @@
 SCRIPT_PATH=$(readlink -f "$0")
 SCRIPT_DIR=$(dirname "$SCRIPT_PATH")
 SRC_DIR=$(cd "$SCRIPT_DIR/.." && pwd)
-adb pull sdcard/DCIM/Camera $SRC_DIR/model/data/
+adb pull sdcard/DCIM/Camera $SRC_DIR/data/
 adb shell rm sdcard/DCIM/Camera/*
